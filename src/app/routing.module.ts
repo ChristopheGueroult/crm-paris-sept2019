c22a51ed25d7ae12aa78a709106bac3de1b2bf59
christophe gueroult <christophegueroult@gmail.com>
2019-09-03 16:33:00 +0200

    mardi 16h30

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, Router } from '@angular/router';
+import { RouterModule, Routes, Router, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './login/pages/login/login.component';
 
 const appRoutes: Routes = [
@@ -28,7 +28,10 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: false } // <-- debugging purposes only
+      {
+        enableTracing: false,
+        preloadingStrategy: PreloadAllModules
+       } // <-- debugging purposes only
     )
   ]
 })
