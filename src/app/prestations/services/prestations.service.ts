9b6190d0c64e404905a46449777305843a0398cc
christophe gueroult <christophegueroult@gmail.com>
2019-09-04 10:32:56 +0200

    service prestations

---
+++ src/app/prestations/services/prestations.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrestationsService {
+
+  constructor() { }
+
+  // get collection
+
+  // set collection
+
+  // update item in collection
+
+  // add item in collection
+
+  // get item by id in collection
+}
