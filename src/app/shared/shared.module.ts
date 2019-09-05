bb3022be432e3d8ee778ec18a72725e105898e3c
christophe gueroult <christophegueroult@gmail.com>
2019-09-05 14:48:03 +0200

    templates for projections

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
 import { TableauComponent } from './components/tableau/tableau.component';
+import { TemplatesModule } from '../templates/templates.module';
 
 @NgModule({
   declarations: [TotalPipe, StateDirective, TableauComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TemplatesModule
   ],
-  exports: [TotalPipe, StateDirective, TableauComponent],
+  exports: [TotalPipe, StateDirective, TableauComponent, TemplatesModule],
 })
 export class SharedModule { }
